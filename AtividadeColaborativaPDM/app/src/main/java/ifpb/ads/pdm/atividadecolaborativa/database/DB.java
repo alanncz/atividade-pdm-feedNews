2c61c7d15093524632c4ad4d493b5683398e977f
alann <alannrodrigues@hotmail.com>
2019-08-16 15:14:37 -0300

    inserir, listar e verificar post no banco de dados

---
+++ AtividadeColaborativaPDM/app/src/main/java/ifpb/ads/pdm/atividadecolaborativa/database/DB.java
@@ -0,0 +1,4 @@
+package ifpb.ads.pdm.atividadecolaborativa.database;
+
+public class DB {
+}
--- AtividadeColaborativaPDM/app/src/main/java/ifpb/ads/pdm/atividadecolaborativa/enums/Sites.java
+++
@@ -1,6 +0,0 @@
-package ifpb.ads.pdm.atividadecolaborativa.enums;
-
-public enum Sites {
-
-    DIARIO_DO_SERTAO, AQUI_CONECTADOS, JORNAL_DA_PARAIBA;
-}
